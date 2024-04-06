6103bb1ac6706913be221b383ebbecfb1a3599d3
pablocurto <153191324+pablocurto@users.noreply.github.com>
2024-04-06 11:53:33 +0000

    Cambio

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -10,20 +10,46 @@ function Semaforo (){
         setColorSem(nuevoColor);
     },[contador]);
     
-    return(
-        <><div className="container">
-            <div className={"light red"+((colorSem === "red") ? " glowRed" : "")}>
+    let clic =1;
+    
+    function añadirMorado(){
+        
 
-            </div>
-            <div className={"light yellow"+((colorSem === "yellow") ? " glowYellow" : "")}>
-                
-            </div>
-            <div className={"light green"+((colorSem === "green") ? " glowGreen" : "")}>
-                
-            </div>
+        if(clic==1){
+            document.querySelector("#luzMorada").style.height = "100px";
+            document.querySelector("#contenedor").style.height = "500px";
+            colores.push("purple")
             
+            clic = clic + 1;
+        }
+        else{
+            document.querySelector("#luzMorada").style.height = "0px";
+            document.querySelector("#contenedor").style.height = "400px";
+            clic = 1;
+        }
+        
+    }
+
+    return(
+        <><div className="container" id="contenedor">
+            <div
+                onClick={()=>setColorSem("red")}
+                className={"light red"+((colorSem === "red") ? " glowRed" : "")}>
+            </div>
+            <div 
+                onClick={()=>setColorSem("yellow")}
+                className={"light yellow"+((colorSem === "yellow") ? " glowYellow" : "")}>
+            </div>
+            <div 
+                onClick={()=>setColorSem("green")}
+                className={"light green"+((colorSem === "green") ? " glowGreen" : "")}>
+            </div>
+            <div id="luzMorada" onClick={()=>setColorSem("purple")}
+                className={"light purple"+((colorSem === "purple") ? " glowPurple" : "")}>
+            </div>
         </div>
         <button className="cambio" onClick={()=>setContador(contador+1)}>Cambiar color</button>
+        <button className="morado" onClick={()=>añadirMorado()}>Añadir div morado</button>
         </>
     )
 }
